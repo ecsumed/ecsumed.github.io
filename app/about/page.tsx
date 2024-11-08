dc362a5ed434c455e6933fabdd8a38104dfd2088
Fahad Saleh <ecsumed@yahoo.com>
2024-11-08 15:57:08 -0500

    Add command to post

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
 
         <div className='p-3 rounded h-50 bg-slate-300 dark:bg-zinc-800 dark:text-white'>
-            <TerminalCommand command={["$ whoami && cat about-me.md | renderhtml"]} delay={1000}>
+            <TerminalCommand command={["$ whoami && cat about-me.md | renderHTML"]} delay={1000}>
                 <p className='text-2xl'>about</p>
             </TerminalCommand>
         </div >
--- app/post/[post]/page.tsx
+++ app/post/[post]/page.tsx
@@ -4,6 +4,7 @@ import { getPost, getSortedPostsData } from '../../../lib/posts';
 import Link from 'next/link';
 import Badge from '@/app/components/badge';
 import Image from 'next/image';
+import { TerminalCommand } from '@/app/components/terminal';
 
 
 
@@ -34,44 +35,47 @@ export default async function Page({
   }
 
   return (
-    <div className="min-h-screen p-2 font-[family-name:var(--font-geist-sans)]">
-      <div className="">
-        <Image width="0"
-          height="0"
-          sizes="100vw"
-          className="w-full h-48" src={`/content/images/${post.img}`} alt="Post header image">
-        </Image>
-      </div>
-
-      <div className='mt-5 mx-5'>
-        <div className="">
-          <span className='text-4xl'>{post.title}</span>
-        </div>
-
-        <div className='grid grid-cols-2'>
-          <div className='content-center'>
-            {post.tags.map((tag: string) => (
-              <Link href={`/posts/${tag}`}>
-                <Badge color="green" text={tag}></Badge>
-              </Link>
-            ))}
+    <div className='p-3 rounded h-50 bg-slate-300 dark:bg-zinc-800 dark:text-white'>
+      <TerminalCommand command={[`$ cat posts/${postID}.md | renderHTML`]} delay={1000}>
+        <div className="min-h-screen p-2 font-[family-name:var(--font-geist-sans)]">
+          <div className="">
+            <Image width="0"
+              height="0"
+              sizes="100vw"
+              className="w-full h-48" src={`/content/images/${post.img}`} alt="Post header image">
+            </Image>
           </div>
 
-          <div className="pr-2 flex justify-end">
-            <span className='text-lg'>{month} {day}, {year} </span>
+          <div className='mt-5 mx-5'>
+            <div className="">
+              <span className='text-4xl'>{post.title}</span>
+            </div>
+
+            <div className='grid grid-cols-2'>
+              <div className='content-center'>
+                {post.tags.map((tag: string) => (
+                  <Link href={`/posts/${tag}`}>
+                    <Badge color="green" text={tag}></Badge>
+                  </Link>
+                ))}
+              </div>
+
+              <div className="pr-2 flex justify-end">
+                <span className='text-lg'>{month} {day}, {year} </span>
+              </div>
+            </div>
+
           </div>
+
+          <div className='mt-6 mb-16 p-6 rounded bg-slate-50 text-black dark:bg-zinc-900 dark:text-white'>
+            <MarkdownRenderer>
+              {post.content}
+            </MarkdownRenderer>
+          </div>
+
         </div>
-
-      </div>
-
-      <div className='mt-6 mb-16 p-6 rounded bg-slate-50 text-black dark:bg-zinc-900 dark:text-white'>
-        <MarkdownRenderer>
-          {post.content}
-        </MarkdownRenderer>
-      </div>
-
-    </div>
-
+      </TerminalCommand>
+    </div >
   );
 }
 
