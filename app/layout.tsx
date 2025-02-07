c2656702f46765220068054933c7895bae235507
Fahad Saleh <ecsumed@yahoo.com>
2025-02-07 15:39:02 -0500

    fix: virtual border to be always show with y-scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,9 +40,9 @@ export default function RootLayout({
 
 
           <div className="h-screen flex flex-col">
-            <div className="grid grid-cols-[3fr_94fr_3fr] mt-8">
+            <div className="grid grid-cols-[3fr_94fr_3fr] mt-8 overflow-y-auto" style={{ scrollbarGutter: "stable" }}>
               <div className=""></div>
-              <div className="col-start-2 pr-3 md:pr-5">
+              <div className="col-start-2 pr-3 md:pr-5 border-solid border-b border-stone-700 dark:border-[#e5e1e1]">
                 <Header></Header>
               </div>
               <div className=""></div>
@@ -50,19 +50,18 @@ export default function RootLayout({
 
             <div className="flex-1 grid grid-cols-[3fr_94fr_3fr] overflow-y-auto" style={{ scrollbarGutter: "stable" }}>
               <div className=""></div>
-              <div className="col-start-2 flex-col border-solid border border-stone-700 dark:border-[#e5e1e1]">
+              <div className="col-start-2 flex-col border-solid border-x border-stone-700 dark:border-[#e5e1e1]">
                 {children}
               </div>
               <div className="sticky max-sm:mr-6">
                 <div className="fixed vertical-text content-end pt-1">
                   <ThemeSwitch></ThemeSwitch>
                 </div>
-
               </div>
             </div>
 
-            <div className="grid grid-cols-[3fr_94fr_3fr] mb-10">
-              <div className="col-start-2">
+            <div className="grid grid-cols-[3fr_94fr_3fr] mb-10 overflow-y-auto" style={{ scrollbarGutter: "stable" }}>
+              <div className="col-start-2 border-solid border-t border-stone-700 dark:border-[#e5e1e1]">
                 <span className="pl-1 font-thin text-sm text-slate-500 dark:text-neutral-400">
                   All code my own. Snippets borrowed here and there. Inspired by <a className="underline" href="https://p5aholic.me/">Keita Yamada</a>
                 </span>
